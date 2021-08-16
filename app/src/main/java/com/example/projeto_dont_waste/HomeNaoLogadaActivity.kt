5acc3bec7f4d4b086a59a4b789f1e542a24092d8
Beatriz <beatriz.ocgl@gmail.com>
2021-08-16 12:06:19 -0300

    Refatoração do código do layout home não logada

---
+++ app/src/main/java/com/example/projeto_dont_waste/HomeNaoLogadaActivity.kt
@@ -0,0 +1,17 @@
+package com.example.projeto_dont_waste
+
+import android.app.StatusBarManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toolbar
+import com.google.android.material.appbar.AppBarLayout
+
+class HomeNaoLogadaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_nao_logada)
+
+
+    }
+}
